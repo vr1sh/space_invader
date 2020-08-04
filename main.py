8a00490a798da7d05333b6626903d521a0e04081
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-08-04 23:04:05 +0530

    added spawning of multiple enemies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import random
 
 pygame.init()
@@ -18,11 +19,19 @@ player_y = 500
 player_x_change = 0
 
 # enemy character
-enemy_image = pygame.image.load("enemy.png")
-enemy_x = random.randint(0, 800)
-enemy_y = random.randint(30, 175)
-enemy_x_change = 7
-enemy_y_change = 40
+enemy_image = []
+enemy_x = []
+enemy_y = []
+enemy_x_change = []
+enemy_y_change = []
+number_of_enemies = 6
+
+for i in range(number_of_enemies):
+    enemy_image.append(pygame.image.load("enemy.png"))
+    enemy_x.append(random.randint(0, 735))
+    enemy_y.append(random.randint(30, 175))
+    enemy_x_change.append(7)
+    enemy_y_change.append(40)
 
 # enemy pew pew
 laser = pygame.image.load("laser.png")
@@ -32,22 +41,43 @@ laser_x_change = 0
 laser_y_change = 15
 laser_state = "charged"
 
+# score
+score_value = 0
+font = pygame.font.Font("freesansbold.ttf", 30)
+text_x = 12
+text_y = 12
+
+
+
 # background
 background = pygame.image.load("background.png")
+
+
+def display_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
 
 def player(x, y):
     screen.blit(player_image, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemy_image, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemy_image[i], (x, y))
 
 
 def shoot(x, y):
     global laser_state
     laser_state = "shoot"
     screen.blit(laser, (x, y + 10))
+
+
+def collision(enemy_x, enemy_y, laser_x, laser_y):
+    distance = math.sqrt((math.pow(enemy_x - laser_x, 2)) + (math.pow((enemy_y - laser_y), 2)))
+    if distance < 30:
+        return True
+    else:
+        return False
 
 
 # game loop
@@ -62,8 +92,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 player_x_change = 6.7
             if event.key == pygame.K_SPACE:
-                laser_x = player_x
-                shoot(laser_x, laser_y)
+                if laser_state == "charged":
+                    laser_x = player_x
+                    shoot(laser_x, laser_y)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player_x_change = 0
@@ -71,26 +102,43 @@ while running:
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
 
+    # player movement
     player_x += player_x_change
+
     if player_x <= 0:
         player_x = 0
     elif player_x >= 736:
         player_x = 736
 
-    enemy_x += enemy_x_change
-    if enemy_x <= 0:
-        enemy_x_change = 7
-        enemy_y += enemy_y_change
-    elif enemy_x >= 736:
-        enemy_x_change = -7
-        enemy_y += enemy_y_change
-    if laser_y <= 20:
+    # enemy movement
+    for i in range(number_of_enemies):
+        enemy_x[i] += enemy_x_change[i]
+        if enemy_x[i] <= 0:
+            enemy_x_change[i] = 7
+            enemy_y[i] += enemy_y_change[i]
+        elif enemy_x[i] >= 736:
+            enemy_x_change[i] = -7
+            enemy_y[i] += enemy_y_change[i]
+
+        # collision
+        collided = collision(enemy_x[i], enemy_y[i], laser_x, laser_y)
+        if collided:
+            laser_y = 480
+            laser_state = "charged"
+            score_value += 1
+            enemy_x[i] = random.randint(0, 735)
+            enemy_y[i] = random.randint(30, 175)
+        enemy(enemy_x[i], enemy_y[i], i)
+
+    # laser movement
+    if laser_y <= 25:
         laser_y = 480
         laser_state = "charged"
-    if laser_state == " shoot":
+
+    if laser_state == "shoot":
         shoot(laser_x, laser_y)
         laser_y -= laser_y_change
 
     player(player_x, player_y)
-    enemy(enemy_x, enemy_y)
+    display_score(text_x, text_y)
     pygame.display.update()
